8852d9261355bbc8a858bf152ca5d326ff141104
tdtorres <92336506+tdtorres@users.noreply.github.com>
2021-10-13 20:45:14 +0200

    Update Player.c

--- Player.c
+++ Player.c
@@ -2,17 +2,54 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdbool.h>
+#include <string.h>
+/////////////////////////////////////////////////
+//Structure of the Player's inventory//
+/////////////////////////////////////////////////
+struct inventory{
+    int id;
+    char nom [20];
+    char type [20];
+    int damage;
+    int resistance;
+    int restoring;
+    int durability;
+    int quantity;
+};
+typedef struct inventory  inventory;
 
+/////////////////////////////////////////////////
+//Structure of the Player//
+/////////////////////////////////////////////////
+struct Player{
+    int level;
+    float xp;
+    int hpMax;
+    float currHp;
+    int currentWeapon;
+    int currentArmor;
+    inventory CurrentInventory;
+
+};
+typedef struct Player Player;
+Player* initPlayer(){
+    Player* Hero;
+    Hero->level =32;
+    Hero->xp= 615;
+    Hero->hpMax=1000;
+    Hero->currHp =921;
+    Hero->currentWeapon=0;
+    Hero->currentArmor=0;
+    printf("le level de ce type est : %d ",Hero->level);
+    return Hero;
+}
 int main() {
-    struct Player{
-        int level;
-        float xp;
-        int HpMax;
-        float currHp;
-        int currentWeapon;
-        int CurrentArmor;
-        int inventory [2][20];
-    };
-    typedef struct Player Player;
+
+    initPlayer();
+    printf("le level de ce type est : %d ",Hero->level);
+
+
+
+
 
 }
